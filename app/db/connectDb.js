dff4cab5d161bffccabc406825688d74f10b13d7
aayush joshi <aayushjoshi1006@gmail.com>
2024-11-24 19:58:46 +0530

    Changes in database connection

--- app/db/connectDb.js
+++ app/db/connectDb.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   const uri =process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error("MONGODB_URI is not defined in environment variables");
+  }
     try {
-      const conn = await mongoose.connect(uri, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
+      const conn = await mongoose.connect(uri)
+      // console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-      console.error(error.message);
+      console.error(`Database connection error: ${error.message}`);
       process.exit(1);
     }
   }
