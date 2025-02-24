e25b22885ccb3d41c28261e0ec33a25a0835f988
Aayush Joshi <aayushjoshi1006@gmail.com>
2025-02-24 09:01:37 +0530

    connect to database at user page

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -2,9 +2,11 @@ import React from "react";
 import PaymentPage from "../componets/PaymentPage";
 import { notFound } from "next/navigation";
 import User from "../models/User";
+import connectDB from "../db/connectDb";
 
 const Username = async ({ params }) => {
   const checkUser = async () => {
+    await connectDB()
     let u = await User.findOne({ username: params.username });
     if (!u) {
       return notFound();
